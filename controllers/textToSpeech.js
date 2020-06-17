a8ff06952df5486b94e5ad77b39bb396ad0be0f0
t0nyhat <elkobrat@protonmail.com>
2020-06-18 01:13:53 +1000

    working liile refactor

--- controllers/textToSpeech.js
+++ controllers/textToSpeech.js
@@ -1,5 +1,4 @@
 const superagent = require('superagent');
-const { Readable } = require('stream');
 const { addWavHeader } = require('../lib/addWavHeader');
 const { START_PCM, END_PCM, MIDDLE_PCM } = require('../auth/consts');
 const { fetchIamToken } = require('../auth/iam-token');
@@ -16,7 +15,7 @@ function textToSpeech(req, res, next) {
       .set('Authorization', `Bearer ${token}`)
       .then((result) => {
         const RESULT_PCM_DV = new DataView(result.body.buffer);
-        const out_buf = new Int16Array(result.body.length / 2);
+        const outBuf = new Int16Array(result.body.length / 2);
         try {
           let count = 0;
           for (let i = 0; i < result.body.length; i += 2) {
@@ -24,18 +23,17 @@ function textToSpeech(req, res, next) {
               count = 0;
             }
             if (i < (result.body.length / 3) * 2) {
-              out_buf[i / 2] = RESULT_PCM_DV.getInt16(i, true) + MIDDLE_PCM[count];
+              outBuf[i / 2] = RESULT_PCM_DV.getInt16(i, true) + MIDDLE_PCM[count];
             } else {
-              out_buf[i / 2] = RESULT_PCM_DV.getInt16(i, true);
+              outBuf[i / 2] = RESULT_PCM_DV.getInt16(i, true);
             }
             count += 1;
           }
 
-          const ss = new Int16Array(START_PCM.length + out_buf.length + END_PCM.length);
+          const ss = new Int16Array(START_PCM.length + outBuf.length + END_PCM.length);
           ss.set(START_PCM);
-          ss.set(out_buf, START_PCM.length);
-          ss.set(END_PCM, START_PCM.length + out_buf.length);
-
+          ss.set(outBuf, START_PCM.length);
+          ss.set(END_PCM, START_PCM.length + outBuf.length);
 
           const str = addWavHeader(ss, 48000, 16, 1);
 
