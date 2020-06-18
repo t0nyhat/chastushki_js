3a14adeba370ab826b38b2f2863ed5e3b85c202d
t0nyhat <elkobrat@protonmail.com>
2020-06-18 10:13:47 +1000

    fix content type

--- controllers/textToSpeech.js
+++ controllers/textToSpeech.js
@@ -3,6 +3,31 @@ const { addWavHeader } = require('../lib/addWavHeader');
 const { START_PCM, END_PCM, MIDDLE_PCM } = require('../auth/consts');
 const { fetchIamToken } = require('../auth/iam-token');
 
+function createDitty(rawPcm) {
+  const RESULT_PCM_DV = new DataView(rawPcm.buffer);
+  const outBuf = new Int16Array(rawPcm.length / 2);
+  let count = 0;
+  for (let i = 0; i < rawPcm.length; i += 2) {
+    if (count > (MIDDLE_PCM.length - 1)) {
+      count = 0;
+    }
+    if (i < (rawPcm.length / 3) * 2) {
+      outBuf[i / 2] = RESULT_PCM_DV.getInt16(i, true) + MIDDLE_PCM[count];
+    } else {
+      outBuf[i / 2] = RESULT_PCM_DV.getInt16(i, true);
+    }
+    count += 1;
+  }
+
+  const fullRawPcmDitty = new Int16Array(START_PCM.length + outBuf.length + END_PCM.length);
+  fullRawPcmDitty.set(START_PCM);
+  fullRawPcmDitty.set(outBuf, START_PCM.length);
+  fullRawPcmDitty.set(END_PCM, START_PCM.length + outBuf.length);
+
+  const fullWavDitty = addWavHeader(fullRawPcmDitty, 48000, 16, 1);
+  return Buffer.from(fullWavDitty);
+}
+
 function textToSpeech(req, res, next) {
   const { text } = req.body;
 
@@ -14,32 +39,10 @@ function textToSpeech(req, res, next) {
       })
       .set('Authorization', `Bearer ${token}`)
       .then((result) => {
-        const RESULT_PCM_DV = new DataView(result.body.buffer);
-        const outBuf = new Int16Array(result.body.length / 2);
-        try {
-          let count = 0;
-          for (let i = 0; i < result.body.length; i += 2) {
-            if (count > (MIDDLE_PCM.length - 1)) {
-              count = 0;
-            }
-            if (i < (result.body.length / 3) * 2) {
-              outBuf[i / 2] = RESULT_PCM_DV.getInt16(i, true) + MIDDLE_PCM[count];
-            } else {
-              outBuf[i / 2] = RESULT_PCM_DV.getInt16(i, true);
-            }
-            count += 1;
-          }
-
-          const ss = new Int16Array(START_PCM.length + outBuf.length + END_PCM.length);
-          ss.set(START_PCM);
-          ss.set(outBuf, START_PCM.length);
-          ss.set(END_PCM, START_PCM.length + outBuf.length);
-
-          const str = addWavHeader(ss, 48000, 16, 1);
-
-          res.send(Buffer.from(str));
-        } catch (err) {
-          next();
+        if (result.ok) {
+          const wavDitty = createDitty(result.body);
+          res.append('Content-Type', 'audio/x-wav');
+          res.send(wavDitty);
         }
       })
       .catch(next);
